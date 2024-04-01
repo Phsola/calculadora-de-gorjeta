d597325d4fa89046a8ae60de8c590094bae350d4
Pedro Vicentini <Pedrohenriquevicentinilp@gmail.com>
2024-03-31 23:26:53 -0300

    nono commit do CSS e terceito commit do JS

--- script.js
+++ script.js
@@ -28,8 +28,17 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelector(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => [
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao)
 ])
 
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
 
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) /100
 }
